a8f0333fc732177f6c10d3e8063a8aa99011e7e8
Natali <natali.rak911@gmail.com>
2020-05-03 15:15:58 +0300

    +section run and carousel

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousell__slide').slick({
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.png"></button>'
+    });
+    
+  });
