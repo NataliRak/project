e0d4540147f8fe2744d177470a3621bd47ec239c
Natali <natali.rak911@gmail.com>
2020-05-05 16:16:06 +0300

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -6,5 +6,12 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.png"></button>'
     });
+
+    $('ul.tab__group').on('click', 'li:not(.tab__name_active)', function() {
+        $(this)
+          .addClass('tab__name_active').siblings().removeClass('tab__name_active')
+          .closest('div.container').find('div.tab__cards').removeClass('tab__cards_active').eq($(this).index()).addClass('tab__cards_active');
+      });
+      
     
   });
